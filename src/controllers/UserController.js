b688d53008839e610428a2ac603f227b18294fab
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-10-19 12:17:46 +1030

    ci: example automation workflow

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -42,6 +42,21 @@ router.get("/:userid", async (request, response) => {
             username: "Pretend this is a username from the DB"
         }
     })
-})
+});
+
+// POST localhost:3000/users/login
+router.post("/login", async (request, response, next) => {
+    let authHeaderData = request.headers["authorization"];
+    console.log(authHeaderData);
+
+    if (authHeaderData != "Example string for header value") {
+        // throw new Error("Not valid login data!");
+        return next(new Error("Not valid login data!"));
+    }
+
+    response.json({
+        authHeaderData
+    });
+});
 
 module.exports = router
--- tests/users.test.js
+++ tests/users.test.js
@@ -51,11 +51,24 @@ describe("Users controller", () => {
 		// POST localhost:3000/users/login
 		const response = await request(app)
 			.post("/users/login")
-			.send({
-				username: "jason", 
-				password:"SuperCoolPassword1"
-			});
+            .set("Authorization", "Example string for header value")
+			// .send({
+			// 	username: "jason", 
+			// 	password:"SuperCoolPassword1"
+			// });
+        expect(response.body.authHeaderData).toBe("Example string for header value")
 	});
+
+    test("User login route throws an error on invalid login data", async () => {
+        // POST localhost:3000/users/login
+		const response = await request(app)
+        .post("/users/login")
+        .set("Authorization", "This should cause an error")
+
+        expect(response.body.authHeaderData).toBeUndefined()
+        expect(response.body.status).toBe(500);
+        expect(response.body.error).toBe("Not valid login data!")
+    })
 
 	test.skip("User update/edit route returns a single user as object", async () => {
 		// PATCH localhost:3000/users/12345
